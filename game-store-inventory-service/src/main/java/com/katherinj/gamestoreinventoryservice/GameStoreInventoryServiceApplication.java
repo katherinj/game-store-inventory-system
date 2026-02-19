074a4e318d24b072294c59fe41d73613bea3ae31
Katherin Jimenez <katjimenez98@gmail.com>
2026-02-18 21:44:01 -0500

    initialize Spring boot backend with JPA, H2, graphql, actuator

---
+++ game-store-inventory-service/src/main/java/com/katherinj/gamestoreinventoryservice/GameStoreInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.katherinj.gamestoreinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStoreInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ game-store-inventory-service/src/test/java/com/katherinj/gamestoreinventoryservice/GameStoreInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katherinj.gamestoreinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
